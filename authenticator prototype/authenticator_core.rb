2a6782211fd50aa1290647d6d35058cb8a5be9e7
Zuvvo <34513398+Zuvvo@users.noreply.github.com>
2019-01-23 15:31:10 +0100

    authenticator

---
+++ authenticator prototype/authenticator_core.rb
@@ -0,0 +1,35 @@
+users = [
+    {username: "max", password: "password1"},
+    {username: "adam", password: "password2"},
+    {username: "margareth", password: "password3"},
+    {username: "jack", password: "password4"},
+    {username: "mary", password: "password5"},
+]
+
+def auth_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record
+    end
+  end
+  "Credentials were not correct."
+end
+
+max_attempts = 3
+attempts = 0
+
+puts "Welcome!"
+25.times {print "-"}
+puts
+while attempts < max_attempts
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+  authentication = auth_user(username,password, users)
+  puts authentication
+#  input = gets.chomp.downcase
+#  break if input == "n"
+  attempts += 1
+end
+puts "You have exceeded the number of attempts" if attempts == 3
